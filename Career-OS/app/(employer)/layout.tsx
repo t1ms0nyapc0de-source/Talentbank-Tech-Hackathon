00d0dc959fd3c99c066ac2f6bf05ea4b5ce31fe3
Timson Yap <t1ms0nyap.c0de@gmail.com>
2026-06-15 17:09:04 +0800

    Delete Career-OS directory

--- Career-OS/app/(employer)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AppShell } from "@/components/layout/app-shell";
-
-export default function EmployerLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AppShell role="employer">{children}</AppShell>;
-}
